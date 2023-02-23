b1067d308f0e191bc7286d95f1abd1187e928034
Palash <parpanipalash@gmail.com>
2023-02-23 16:26:32 +0000

    DevB comment added

--- demo.java
+++ demo.java
@@ -2,5 +2,8 @@ public class Demo{
 	Public static void main(String args[]{
 		// Print statement for hello world
 		System.out.println("Hello World")
+                // DevB statement for Hello World
 	}
 }
+
+
