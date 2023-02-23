1e58f170bbbbb94684aa67df9087773a8175ab91
Palash <parpanipalash@gmail.com>
2023-02-23 16:30:13 +0000

    demo.java

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class Demo{
 	Public static void main(String args[]{
 		// Print statement for hello world
 		System.out.println("Hello World")
+	        //DevA Print Statement for Hello World
 	}
 }
